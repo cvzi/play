ca49e2bf1ed7f3679cbd8ff27b185007f79aba88
cvzi <cuzi@openmail.cc>
2022-10-05 21:42:20 +0200

    Initial commit

---
+++ docs/main.js
@@ -0,0 +1,139 @@
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', main)
+} else {
+  main()
+}
+
+function main (ev) {
+  if (document.location.protocol !== 'https:' && document.location.hostname.indexOf('heroku') !== -1) {
+    document.location.protocol = 'https:'
+  }
+  if (!document.getElementById('appid').value) {
+    document.getElementById('appid').value = document.getElementById('appid').placeholder.split(' ')[0]
+  }
+  document.getElementById('baseurl').value = document.location.origin + document.getElementById('basepath').value
+  document.getElementById('baseurl').addEventListener('change', updateUrls)
+  document.getElementById('baseendpoint').addEventListener('change', updateUrls)
+  document.getElementById('appid').addEventListener('change', updateUrls)
+  document.getElementById('label').addEventListener('change', updateUrls)
+  document.getElementById('message').addEventListener('change', updateUrls)
+  document.getElementById('baseurl').addEventListener('keyup', updateUrls)
+  document.getElementById('baseendpoint').addEventListener('keyup', updateUrls)
+  document.getElementById('appid').addEventListener('keyup', updateUrls)
+  document.getElementById('label').addEventListener('keyup', updateUrls)
+  document.getElementById('message').addEventListener('keyup', updateUrls)
+  document.getElementById('shieldurl').addEventListener('change', updateImages)
+  document.getElementById('shieldurlstyled').addEventListener('change', updateImages)
+  document.getElementById('go').addEventListener('click', function (ev) { ev.preventDefault(); updateUrls(ev)})
+  document.querySelectorAll('.copy').forEach(el => el.addEventListener('click', copyMenu))
+  document.getElementById('copymenu').addEventListener('click', ev => ev.stopPropagation())
+  document.body.addEventListener('click', function() {
+    const m = document.getElementById('copymenu')
+    if(m) m.style.display = 'none'
+  })
+  updateUrls()
+  toggleMobile()
+}
+
+function composeJsonUrl() {
+  const base = document.getElementById('baseurl').value
+  const endpoint = document.getElementById('baseendpoint').value
+  let appid = document.getElementById('appid').value
+  const label = document.getElementById('label').value
+  const message = document.getElementById('message').value
+
+  if (appid.indexOf('?') !== -1) {
+    appid = appid.split('id=')[1].split('&')[0]
+  }
+
+  const jsonurl = base + 'i=' + encodeURIComponent(appid) + '&l=' + encodeURIComponent(label).replace('%24', '$') + '&m=' + encodeURIComponent(message).replace('%24', '$')
+
+  return jsonurl
+}
+var lastTimeout
+function updateUrls () {
+  window.clearTimeout(lastTimeout)
+  const jsonurl = composeJsonUrl()
+  const shieldurl = document.getElementById('baseendpoint').value + 'url=' + encodeURIComponent(jsonurl)
+  const shieldurlstyled = document.getElementById('baseendpoint').value + 'color=green&logo=google-play&logoColor=green&url=' + encodeURIComponent(jsonurl)
+  document.getElementById('jsonurl').value = jsonurl
+  document.getElementById('shieldurl').value = shieldurl
+  document.getElementById('shieldurlstyled').value = shieldurlstyled
+
+  const updateDelayed = function() {
+    updateImages()
+    fetch(jsonurl).then(response => response.text()).then(function(text) {
+      document.getElementById('json').innerHTML = text
+    }).catch(function(e) {
+      document.getElementById('json').innerHTML = 'Could not load JSON:\n' + e
+    })
+  }
+  lastTimeout = window.setTimeout(updateDelayed, 1000)
+}
+
+function updateImages () {
+  document.getElementById('shield').src = document.getElementById('shieldurl').value
+  document.getElementById('shieldstyled').src = document.getElementById('shieldurlstyled').value
+}
+
+
+const copyMap = {
+ 'Copy Badge URL': '$url',
+ 'Copy Markdown': '![Custom badge]($url)',
+ 'Copy reStructuredText': '.. image:: $url   :alt: Custom badge',
+ 'Copy AsciiDoc': 'image:$url[Custom badge]',
+ 'Copy HTML': '<img alt="Custom badge" src="$url">',
+}
+
+function copyMenu (ev) {
+  ev.stopPropagation()
+  const div = document.getElementById('copymenu')
+  const ul = div.querySelector('ul')
+  const copy = function () {
+    const self = this
+    const textarea = document.getElementById(div.dataset.textareaid)
+    const url = textarea.value
+    const text = copyMap[self.textContent].replace(/\$url/g, url)
+    textarea.value = text
+    textarea.focus()
+    textarea.select()
+    const r = document.execCommand('copy')
+    if (r) {
+      textarea.value = url
+      div.parentNode.parentNode.querySelector('.status').innerHTML = ' - Copied!'
+    } else {
+      div.parentNode.parentNode.querySelector('.status').innerHTML = ' - Clipboard not supported on this browser!'
+    }
+    window.setTimeout(() => div.parentNode.parentNode.querySelector('.status').innerHTML = '', 2000)
+  }
+  if (!('textareaid' in div.dataset)) {
+    // Init
+    for(let key in copyMap) {
+      const li = ul.appendChild(document.createElement('li'))
+      li.appendChild(document.createTextNode(key))
+      li.addEventListener('click', copy)
+    }
+  }
+  div.dataset.textareaid = this.parentNode.querySelector('textarea').id
+  div.style.display = 'block'
+  this.parentNode.appendChild(div)
+}
+
+function toggleMobile () {
+  const isMobile = navigator.userAgent.match(/mobile/i)
+  if (isMobile) {
+    document.querySelectorAll('.desktop').forEach(function (e) { e.style.display = 'none' })
+    document.querySelectorAll('.mobile').forEach(function (e) { e.style.display = '' })
+    document.getElementById('jsonurl').size = document.getElementById('jsonurl').dataset.mobileSize
+    document.querySelectorAll('.controls tr').forEach(function (tr) {
+      const tds = tr.querySelectorAll('td')
+      if (tds.length > 1) {
+        tr.parentNode.insertBefore(tr.cloneNode(), tr).appendChild(tds[0])
+      }
+    })
+  } else {
+    document.querySelectorAll('.desktop').forEach(function (e) { e.style.display = '' })
+    document.querySelectorAll('.mobile').forEach(function (e) { e.style.display = 'none' })
+    document.getElementById('jsonurl').size = document.getElementById('jsonurl').dataset.desktopSize
+  }
+}
---
+++ worker.js
@@ -0,0 +1,229 @@
+/*
+https://github.com/cvzi/play/
+
+Copyright (C) 2022 cuzi
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Provides a badge for apps on the Google Play Store
+The program parses the Play Store website and provides
+data for https://shields.io/endpoint
+
+This is run as a Cloudflare worker
+If a KV storage is bound to the variable PLAY_CACHE, it will be
+used to cache requests to play.google.com.
+
+Example: https://play.cuzi.workers.dev/play?i=org.mozilla.firefox&l=Android&m=$version
+Badge: https://img.shields.io/endpoint?url=https%3A%2F%2Fplay.cuzi.workers.dev%2Fplay%3Fi%3Dorg.mozilla.firefox%26l%3DAndroid%26m%3D%24version
+
+*/
+
+/* global addEventListener, Event, Response, fetch, PLAY_CACHE */
+
+const appIDPattern = /[a-zA-Z0-9_]+\.[a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)*/
+
+const playStorePlaceHolders = {
+  $version: 'App version',
+  $installs: 'Installs',
+  $intinstalls: 'Precise installs',
+  $shortinstalls: 'Shorter installs',
+  $updated: 'Last update',
+  $android: 'Required min. Android version',
+  $targetandroid: 'Target Android version',
+  $minsdk: 'Required min. SDK',
+  $targetsdk: 'Target SDK',
+  $rating: 'Rating',
+  $floatrating: 'Precise rating',
+  $name: 'Name',
+  $friendly: 'Content Rating',
+  $published: 'First published'
+}
+
+const fetchConfig = {
+  cf: {
+    // Always cache this fetch regardless of content type
+    // for a max of 1 day before revalidating the resource
+    cacheTtl: 60 * 60 * 24,
+    cacheEverything: true
+  }
+}
+
+const responseConfigJSON = {
+  headers: {
+    'content-type': 'application/json; charset=utf-8',
+    'source-code': 'gist.github.com/cvzi/e5d10613e50d2c4283c97fa1a861933e'
+  }
+}
+
+const responseConfigHTML = {
+  headers: {
+    'content-type': 'text/html; charset=utf-8'
+  }
+}
+
+async function cachedFetchText (url, fetchConfig, event) {
+  if (PLAY_CACHE) {
+    let data = await PLAY_CACHE.get(url, { type: 'text' })
+    if (!data) {
+      data = await (await fetch(url, fetchConfig)).text()
+      if (event instanceof Event) {
+        event.waitUntil(PLAY_CACHE.put(url, data, { expirationTtl: 1800 }))
+      } else {
+        await PLAY_CACHE.put(url, data, { expirationTtl: 1800 })
+      }
+    } else {
+      console.log('Cache hit: ' + url)
+    }
+    return data
+  } else {
+    return (await fetch(url, fetchConfig)).text()
+  }
+}
+
+function replaceVars (text, templateVars) {
+  const escapeRegExp = (s) => s.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+  for (const name in templateVars) {
+    text = text.replace(new RegExp('\\{\\{\\s*' + escapeRegExp(name) + '\\s*\\}\\}', 'gm'), templateVars[name])
+  }
+  return text
+}
+
+async function template (templateUrl, templateVars) {
+  let html = await (await fetch(templateUrl, fetchConfig)).text()
+
+  html = html.replace(/\{\{items:(\w+)\}\}([\s\S]*?)\{\{end\}\}/gm, function (wholeMatch, name, body) {
+    if (name in templateVars) {
+      const result = []
+      for (const key in templateVars[name]) {
+        result.push(replaceVars(body, Object.assign({}, templateVars, { $k: key, $v: templateVars[name][key] })))
+      }
+      return result.join('\n')
+    } else {
+      return wholeMatch
+    }
+  })
+
+  html = replaceVars(html, templateVars)
+
+  return new Response(html, responseConfigHTML)
+}
+
+function replacePlaceHolders (str, app) {
+  for (const placeholder in playStorePlaceHolders) {
+    str = str.replace(placeholder, app[placeholder.substring(1)])
+  }
+  return str
+}
+
+async function getPlayStore (packageName, event) {
+  const url = `https://play.google.com/store/apps/details?id=${encodeURIComponent(packageName)}&gl=US`
+  const content = await cachedFetchText(url, fetchConfig, event)
+
+  const parts = content.split('AF_initDataCallback({').slice(1).map(v => v.split('</script>')[0])
+  const data = parts.filter(s => s.indexOf(`["${packageName}"],`) !== -1)[0].trim()
+  let arr = data.split('data:', 2)[1].split('sideChannel:')[0].trim()
+  arr = arr.substring(0, arr.length - 1) // remove trailing comma
+  const json = JSON.parse(arr)
+
+  const fallback = 'Varies with device'
+
+  const result = {
+    name: json[1][2][0],
+    installs: json[1][2][13][0],
+    intinstalls: json[1][2][13][2],
+    shortinstalls: json[1][2][13][3],
+    version: json[1][2][140][0][0] || fallback,
+    updated: json[1][2][145][0][0] || fallback,
+    targetandroid: json[1][2][140][1][0][0][1] || fallback,
+    targetsdk: json[1][2][140][1][0][0][0] || fallback,
+    android: json[1][2][140][1][1][0][0][1] || fallback,
+    minsdk: json[1][2][140][1][1][0][0][0] || fallback,
+    rating: json[1][2][51][0][0],
+    floatrating: json[1][2][51][0][1],
+    friendly: json[1][2][9][0],
+    published: json[1][2][10][0] || fallback
+  }
+
+  return result
+}
+
+function errorJSON (message) {
+  return new Response(JSON.stringify({
+    schemaVersion: 1,
+    label: 'error',
+    message: message,
+    isError: true
+  }), responseConfigJSON)
+}
+
+async function handleBadge (event, url) {
+  const appId = url.searchParams.get('i') || url.searchParams.get('id') || ''
+
+  if (!appId) {
+    return errorJSON('missing app id')
+  }
+
+  const m = appId.match(appIDPattern)
+  if (!m || !m[0]) {
+    return errorJSON('invalid app id format')
+  }
+  const playData = await getPlayStore(m[0], event)
+
+  let label = url.searchParams.get('l') || url.searchParams.get('label') || 'play'
+  let message = url.searchParams.get('m') || url.searchParams.get('message') || '$version'
+
+  if (label.length > 1000) {
+    label = label.substring(0, 1000)
+  }
+  if (message.length > 1000) {
+    message = message.substring(0, 1000)
+  }
+
+  message = replacePlaceHolders(message, playData)
+
+  label = replacePlaceHolders(label, playData)
+
+  return new Response(JSON.stringify({
+    schemaVersion: 1,
+    label: label,
+    message: message,
+    cacheSeconds: 3600
+  }), responseConfigJSON)
+}
+
+function handleIndex (url) {
+  const templateVars = {
+    appid: url.searchParams.get('i') || url.searchParams.get('id') || 'org.mozilla.firefox',
+    label: url.searchParams.get('l') || url.searchParams.get('label') || 'Android',
+    message: url.searchParams.get('m') || url.searchParams.get('message') || '$version',
+    placeHolders: playStorePlaceHolders
+  }
+
+  return template('https://cvzi.github.io/play/index.html', templateVars)
+}
+
+async function handleRequest (request, event) {
+  const url = new URL(request.url)
+  if (url.pathname.startsWith('/play')) {
+    return handleBadge(event, url)
+  } else if (url.pathname.startsWith('/favicon')) {
+    return Response.redirect('https://cvzi.github.io/play/favicon.ico', 301)
+  } else {
+    return handleIndex(url)
+  }
+}
+
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
